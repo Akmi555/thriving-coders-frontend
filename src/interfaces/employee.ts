8ba4d65ce52363f1a9c71f741685a1038c6bfa88
andrej-naumov <andrejs@list.ru>
2024-03-21 22:57:08 +0100

    minor change

--- src/interfaces/employee.ts
+++ src/interfaces/employee.ts
@@ -2,7 +2,7 @@ export type Employee = {
     employeeId: number
     firstName: string
     lastName: string
+    contactInfo: string
     position: string
     hourlyRate: number
-    contactInfo: string
 }
