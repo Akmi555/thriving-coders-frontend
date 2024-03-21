ee6513204a172496731dc9ada2c06e7f5749e91e
andrej-naumov <andrejs@list.ru>
2024-03-21 17:39:51 +0100

    added fields to add new employee form

--- src/pages/employees/add/employee.tsx
+++ src/pages/employees/add/employee.tsx
@@ -1,9 +1,15 @@
 import Head from 'next/head'
+import { Formik, Form, Field } from 'formik'
+import { mdiAccount, mdiAccountMultipleOutline, mdiAccountOutline, mdiAccountPlus, mdiMail } from '@mdi/js'
+
 import SectionMain from '../../../components/Section/Main'
 import Button from '../../../components/Button'
 import SectionTitleLineWithButton from '../../../components/Section/TitleLineWithButton'
 import { getPageTitle } from '../../../config'
-import { mdiAccountMultipleOutline, mdiAccountPlus } from '@mdi/js'
+import CardBox from '../../../components/CardBox'
+import Buttons from '../../../components/Buttons'
+import Divider from '../../../components/Divider'
+import FormField from '../../../components/Form/Field'
 
 const AddNewEmployeePage = () => {
     return (
@@ -23,6 +29,59 @@ const AddNewEmployeePage = () => {
                         small
                     />
                 </SectionTitleLineWithButton>
+
+
+                <CardBox>
+                    <Formik
+                        initialValues={{
+                            fullname: '',
+                            email: '',
+                            phone: '',
+                            position: 'engineer',
+                            textarea: 'Hello',
+                        }}
+                        onSubmit={(values) => alert(JSON.stringify(values, null, 2))}
+                    >
+                        <Form>
+                            <FormField label="Please insert first and last name" icons={[ mdiAccountOutline, mdiAccount ]}>
+                                <Field name="firstname" placeholder="Full name" />
+                                <Field name="lastName" placeholder="Last name" />
+                            </FormField>
+
+                            <FormField
+                                label="Provide Your contact information"
+                                labelFor="contact"
+                                help="Phone numbers or email addresses"
+                            >
+                                <Field name="contact" placeholder="Contact" id="contact" />
+                            </FormField>
+
+                            <FormField label="Select position and hourly rate" labelFor="position">
+                                <Field name="position" id="position" component="select">
+                                    <option value="designer">Designer</option>
+                                    <option value="assistant">Assistant</option>
+                                    <option value="engineer">Engineer</option>
+                                    <option value="architect">Architect</option>
+                                </Field>
+                                <Field name="hourlyRate" placeholder="Hourly rate" />
+                            </FormField>
+
+                            <Divider />
+
+                            <FormField label="Textarea" hasTextareaHeight>
+                                <Field name="textarea" as="textarea" placeholder="Your text here" />
+                            </FormField>
+
+                            <Divider />
+
+                            <Buttons>
+                                <Button type="submit" color="info" label="Submit" />
+                                <Button type="reset" color="info" outline label="Reset" />
+                            </Buttons>
+                        </Form>
+                    </Formik>
+                </CardBox>
+
             </SectionMain>
         </>
 
