f952c6124d5fd95672c4be871cc5074556d69e6e
andrej-naumov <andrejs@list.ru>
2024-03-22 01:47:31 +0100

    added errors array for form fields

--- src/components/Form/Field.tsx
+++ src/components/Form/Field.tsx
@@ -6,13 +6,14 @@ type Props = {
   labelFor?: string
   help?: string
   icons?: string[] | null[]
+  errors?: string[] | null
   isBorderless?: boolean
   isTransparent?: boolean
   hasTextareaHeight?: boolean
   children: ReactNode
 }
 
-const FormField = ({ icons = [], ...props }: Props) => {
+const FormField = ({ errors = [], icons = [], ...props }: Props) => {
   const childrenCount = Children.count(props.children)
 
   let elementWrapperClass = ''
@@ -23,6 +24,7 @@ const FormField = ({ icons = [], ...props }: Props) => {
       break
     case 3:
       elementWrapperClass = 'grid grid-cols-1 gap-3 md:grid-cols-3'
+    // case 4 ???  
   }
 
   const controlClassName = [
@@ -31,7 +33,9 @@ const FormField = ({ icons = [], ...props }: Props) => {
     props.hasTextareaHeight ? 'h-24' : 'h-12',
     props.isBorderless ? 'border-0' : 'border',
     props.isTransparent ? 'bg-transparent' : 'bg-white dark:bg-slate-800',
+    errors && errors.length > 0 ? 'border-pink-600' : '',
   ].join(' ')
+
 
   return (
     <div className="mb-6 last:mb-0">
@@ -47,7 +51,13 @@ const FormField = ({ icons = [], ...props }: Props) => {
         {Children.map(props.children, (child: ReactElement, index) => (
           <div className="relative">
             {cloneElement(child, {
-              className: `${controlClassName} ${icons[index] ? 'pl-10' : ''}`,
+              className: `px-3 py-2 max-w-full rounded w-full dark:placeholder-gray-400
+            focus:ring focus:ring-blue-600 focus:border-blue-600 focus:outline-none
+            ${props.hasTextareaHeight ? 'h-24' : 'h-12'}
+            ${props.isBorderless ? 'border-0' : 'border'}
+            ${props.isTransparent ? 'bg-transparent' : 'bg-white dark:bg-slate-800'}
+            ${errors && errors[index] ? 'border-pink-600' : 'border-gray-700'} 
+            ${icons[index] ? 'pl-10' : ''}`,
             })}
             {icons[index] && (
               <Icon
@@ -57,9 +67,13 @@ const FormField = ({ icons = [], ...props }: Props) => {
                 className="absolute top-0 left-0 z-10 pointer-events-none text-gray-500 dark:text-slate-400"
               />
             )}
+            {errors && errors[index] && (
+              <div className="text-sm text-red-500 dark:text-red-400 mt-1">{errors[index]}</div>
+            )}
           </div>
         ))}
       </div>
+
       {props.help && (
         <div className="text-xs text-gray-500 dark:text-slate-400 mt-1">{props.help}</div>
       )}
