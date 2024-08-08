b267f4974c6d8db072ae8443327e7afb0d6153f7
Kristina Deriugina <derkristina258@gmail.com>
2024-08-08 12:01:50 +0100

    fixed mistake Customer

--- src/pages/customers/add/addNewCustomer.ts
+++ src/pages/customers/add/addNewCustomer.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const addNewCustomer = async (customerData) => {
   try {
     const response = await axios.post('http://localhost:4567/customers', customerData);
-    console.log('Employee added successfully:', response.data);
+    console.log('Customer added successfully:', response.data);
     return response;
   } catch (error) {
     console.error('Error adding customer:');
