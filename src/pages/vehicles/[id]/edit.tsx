43cab8c1153ea3b0bc4ea3fcdc283e24b7fa3f4d
Galina Lasko <galina_lasko@hotmail.com>
2024-08-11 22:25:22 +0200

    added changes

--- src/pages/vehicles/[id]/edit.tsx
+++ src/pages/vehicles/[id]/edit.tsx
@@ -4,8 +4,9 @@ import SectionMain from 'components/Section/Main'
 import SectionTitleLineWithButton from 'components/Section/TitleLineWithButton'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { getPageTitle } from 'src/config'
+import { fetchOneVehicle } from 'src/hooks/vehicleData'
 
 const EditVehicle = () => {
   const [vehicleData, setVehicleData] = useState({
@@ -17,7 +18,44 @@ const EditVehicle = () => {
   const { id } = router.query;
 
   console.log('vehicle id = ' + id); // TODO - удалить
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await fetchOneVehicle(id);
+        console.log(result);
+        setVehicleData({
+          vehicle: result.vehicle,
+          isLoading: false,
+          isError: result.isError,
+        });
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        setVehicleData({
+          vehicle: null,
+          isLoading: false,
+          isError: true,
+        });
+      }
+    };
+
+    if (id) {
+      fetchData();
+    }
+  }, [id]);
+
+  if (vehicleData.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (vehicleData.isError) {
+    return <div>Error loading vehicle data</div>;
+  }
+
+  const vehicle = vehicleData.vehicle;
+
   return (
+    
     <>
       <Head>
         <title>{getPageTitle('Edited vehicle')}</title>
