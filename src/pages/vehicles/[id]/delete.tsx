585408657789f98b23592e6a1e2f5a865e2f3c36
Galina Lasko <galina_lasko@hotmail.com>
2024-08-11 22:24:17 +0200

    Added dialogue of deleting vehicle

--- src/pages/vehicles/[id]/delete.tsx
+++ src/pages/vehicles/[id]/delete.tsx
@@ -1,42 +1,70 @@
-import  { useRouter } from "next/router";
-import { ReactElement, useEffect } from "react";
-import deleteVehicleAsync from "../add/deleteVehicleAsync";
-import LayoutAuthenticated from "src/layouts/Authenticated";
-import VehiclesPage from "..";
-import Button from "components/Button";
-
+import { useRouter } from 'next/router'
+import { ReactElement, useEffect, useState } from 'react'
+import deleteVehicleAsync from '../add/deleteVehicleAsync'
+import LayoutAuthenticated from 'src/layouts/Authenticated'
+import VehiclesPage from '..'
+import Button from 'components/Button'
+import CardBoxModal from 'components/CardBox/Modal'
+import { getPageTitle } from 'src/config'
+import Head from 'next/head'
+import SectionMain from 'components/Section/Main'
+import SectionTitleLineWithButton from 'components/Section/TitleLineWithButton'
+import { mdiCar } from '@mdi/js'
 
 const DeleteVehicle = () => {
-    const router = useRouter();
-    const {id} = router.query;
-
-    useEffect(()=>{
-
-        const deleteVehicle = async () => {deleteVehicleAsync(id)}
-
-        if(id) {
-            deleteVehicle();
-        }
-
-    }, [id, router]
-
-    )
-    
-
-    return (
-        <>
-            <div>
-                <h1>Deleting Vehicle {id} </h1>
-                <p>Click to return dashboard ...link</p>
-            </div>
-            
-        
-        </>
-    )
-    
-}
-DeleteVehicle.getLayout = function getLayout(page: ReactElement) {
-    return <LayoutAuthenticated>{page}</LayoutAuthenticated>
+  const router = useRouter()
+  const { id } = router.query
+  //
+  const deleteVehicle = async () => {
+    deleteVehicleAsync(id)
   }
+  const handleDeleteAction = () => {
+    deleteVehicle()
+    setIsModalInfoActive(false)
+  }
+  const handleCancelAction = () => {
+    setIsModalInfoActive(false)
+  }
+  const [isModalInfoActive, setIsModalInfoActive] = useState(true)
+  const modalSampleContents = (
+    <>
+      <p>
+        Would you like to delete this vehicle <b>really delete?</b>
+      </p>
+      <p>In this case you will not be able to return it back</p>
+    </>
+  )
+
+  return (
+    <>
+      <Head>
+        <title>{getPageTitle('Delete vehicle')}</title>
+      </Head>
+      <SectionMain>
+        <SectionTitleLineWithButton icon={mdiCar} title="Delete specific vehicle" main>
+          <Button
+            href={`/vehicles/${id}`}
+            // target="_blank"
+            icon={mdiCar}
+            label="Back to specific vehicle"
+            color="contrast"
+            roundedFull
+            small
+          />
+        </SectionTitleLineWithButton>
+        <CardBoxModal
+          title="Please confirm action"
+          buttonColor="danger"
+          buttonLabel="Delete"
+          isActive={isModalInfoActive}
+          onConfirm={handleDeleteAction}
+          onCancel={handleCancelAction}
+        >
+          {modalSampleContents}
+        </CardBoxModal>
+      </SectionMain>
+    </>
+  )
+}
 
 export default DeleteVehicle
