c08c8cfc14a61815b5f47d8b6bb701b9ffce908e
Kristina Deriugina <derkristina258@gmail.com>
2024-08-14 23:15:38 +0200

    changed enpoint customers

--- src/pages/customers/add/editCustomerAsync.ts
+++ src/pages/customers/add/editCustomerAsync.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const editCustomerAsync = async (customerData) => {
     try{
-        const response = await axios.post('http://localhost:4567/customers/id', customerData);
+        const response = await axios.put('http://localhost:4567/customers', customerData);
         console.log('Customer added successfully:', response.data);
         return response;
       } catch (error) {
