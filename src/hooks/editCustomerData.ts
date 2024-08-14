13eda4d05450701f2ff41887e7ed63a965e1c6a9
Kristina Deriugina <derkristina258@gmail.com>
2024-08-14 21:32:52 +0200

    added EditCustomerData logik

---
+++ src/hooks/editCustomerData.ts
@@ -0,0 +1,23 @@
+import useSWR from 'swr';
+
+//   const fetcher = (url: string) => fetch(url).then((res) => res.json());
+const fetcher = (url: string): Promise<any> => 
+    fetch(url).then((res) => res.json());
+
+export const fetchAllCustomers= async () => {
+    try {
+        // const response = await fetcher('https://api.thriving-coders.com/customers');
+        const response = await fetcher('http://localhost:4567/customers');
+        return {
+            customers: response ?? [],
+            isLoading: false,
+            isError: false,
+        };
+    } catch (error) {
+        return {
+            customers: [],
+            isLoading: false,
+            isError: true,
+        };
+    }
+}
